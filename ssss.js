3741d0ba8b93d293027aac14a1839617dd27de6a
ali@purple7.com <ali@purple7.com>
2019-03-01 13:43:22 +0000

    Added recreation method

--- ssss.js
+++ ssss.js
@@ -2,18 +2,18 @@ var secrets = require('secrets.js-grempe');
 var BIP39 = require('bip39');
 console.clear()
 //generate mnemomic
-var mnemonic = BIP39.generateMnemonic(160)
-console.log(`\n\n15 word mnemonic: ` + mnemonic);
-console.log(`\n\nCharactor length of mnemonic: ${mnemonic.length}`)
+var mnemonic = BIP39.generateMnemonic(256)
+console.log(`\n\n24 word mnemonic: ` + mnemonic);
+console.log(`\nCharactor length of mnemonic: ${mnemonic.length}`)
 //converting to hex
 mnemonicHex = secrets.str2hex(mnemonic);
-console.log(`\n\nCharactor length of mnemonic in hex: ${(secrets.str2hex(mnemonic).length)}`)
+console.log(`\nCharactor length of mnemonic in hex: ${(secrets.str2hex(mnemonic).length)}`)
 
 //devide into n of m shares
 var threshold=3
 var n=4
 
-console.log(`\n\nDeviding in to ${n} shares with a threshold of ${threshold}: `)
+console.log(`\nDeviding in to ${n} shares with a threshold of ${threshold}: `)
 var shares = secrets.share(secrets.str2hex(mnemonic), n, threshold)
 for(i=0;i<n;i++)    {
     console.log(`\nshare ${i+1}, Hexadecimal: ` + shares[i]);
@@ -22,3 +22,10 @@ for(i=0;i<n;i++)    {
     console.log(`\nshare ${i+1}, 16 bit string: ` + secrets.hex2str(shares[i]));
     console.log(`\nshare ${i+1}, 16 bit string length: ` + (secrets.hex2str(shares[i])).length);
 }
+
+//combining 3 shares to re-create the original mnemonic
+var receatedMnemonic = secrets.combine([shares[0], shares[2],shares[3]])
+
+console.log(`\nRecreating mnemonic with share 1, 3 and 4`)
+console.log(`\nRecreated mnemonic: ${secrets.hex2str(receatedMnemonic)}`)
+console.log(`\nOriginal mnemonic: ${mnemonic}`)
